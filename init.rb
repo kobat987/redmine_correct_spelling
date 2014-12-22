64bf81f6392ac967d9b80bf67f0e48675882e31d
kobat987 <kobat987@gmail.com>
2014-12-22 11:24:51 +0900

    スケルトン生成し情報記述
    スケルトンを生成した後、init.rbにプラグイン情報を記述し、Redmineに
    認識されるところまで作成した。

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_correct_spelling do
+  name 'Redmine Correct Spelling plugin'
+  author 'kobat987'
+  description 'This is a plug-in to create a glossary of correct spelling in Redmine.'
+  version '0.0.1'
+  url 'https://github.com/kobat987/'
+  author_url 'http://otsukare-tion.com/'
+end
