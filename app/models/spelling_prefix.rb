1c84aef1ae057c40532cfa574e5cd56baeb3a641
kobat987 <kobat987@gmail.com>
2015-01-07 17:02:02 +0900

    微修正
    OK表記の必須化

--- app/models/spelling_prefix.rb
+++ app/models/spelling_prefix.rb
@@ -2,6 +2,7 @@ class SpellingPrefix < ActiveRecord::Base
   unloadable
 
   validates_presence_of :yomi
+  validates_presence_of :spell_ok
   validates_length_of :spell_ok, :maximum => 255
   validates_length_of :spell_ng, :maximum => 255
   validates_length_of :example, :maximum => 255
