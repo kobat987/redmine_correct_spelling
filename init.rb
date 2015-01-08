4737e90aae35440bca922aca79a057b830658b5a
kobat987 <kobat987@gmail.com>
2015-01-08 22:17:16 +0900

    README.rdocとinit.rbの作成

--- init.rb
+++ init.rb
@@ -1,11 +1,11 @@
 Redmine::Plugin.register :redmine_correct_spelling do
   name 'Redmine Correct Spelling plugin'
-  author 'kobat987'
+  author 'kobat'
   description 'This is a plug-in to create a glossary of correct spelling in Redmine.'
-  version '0.0.1'
-  url 'https://github.com/kobat987/'
+  version '1.0.0'
+  url 'https://github.com/kobat987/redmine_correct_spelling/'
   author_url 'http://otsukare-tion.com/'
-  
+
   project_module :spelling do
   permission :view_spelling_prefix, :spelling_prefixes => [:index]
   permission :manage_spelling_prefix,
